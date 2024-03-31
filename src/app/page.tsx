39deb3ebe4bc372cc10534272a23323b55349213
ashwwwin <73720705+ashwwwin@users.noreply.github.com>
2024-04-01 03:44:32 +0700

    improved logo/icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         <title>Liquidify.gg</title>
 
         <div className="w-full items-center px-3 shadow-xl border-b-2 border-white border-opacity-5 py-3.5 flex bg-white bg-opacity-5">
-          <img src="/icon.png" className="h-[35px] select-none mr-3" />
+          <img src="/icon.png" className="h-[35px] select-none" />
           <img
             src="/logo.png"
             className="h-[30px] select-none pointer-events-none"
