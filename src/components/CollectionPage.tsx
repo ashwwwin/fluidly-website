3f2809e7aa79000204015c81085f51d7af73a3fc
ashwwwin <73720705+ashwwwin@users.noreply.github.com>
2024-04-01 09:18:37 +0700

    remove dextools link

--- src/components/CollectionPage.tsx
+++ src/components/CollectionPage.tsx
@@ -421,20 +421,20 @@ const CollectionPage = ({ collections }: { collections: any }) => {
                         `https://app.uniswap.org/#/swap?theme=dark&inputCurrency=ETH&outputCurrency=${collection.liquidifyContract}`
                       );
                     }}
-                    className="flex w-[70px] max-w-[70px] bg-white transition-all duration-[100ms] px-3.5 items-center justify-center hover:bg-opacity-[17.5%] cursor-pointer select-none flex bg-opacity-10 select-none border-y-2 border-opacity-10 border-white"
+                    className="flex w-[70px] max-w-[70px] bg-white transition-all duration-[100ms] px-3.5 items-center justify-center hover:bg-opacity-[17.5%] cursor-pointer select-none flex bg-opacity-10 select-none border-r-2 border-y-2 rounded-r-lg border-opacity-10 border-white"
                   >
                     <img src="/uniswap.png" className="h-[21px]" />
                   </div>
-                  <div
+                  {/* <div
                     onClick={() => {
                       window.open(
-                        "https://www.dextools.io/app/en/ether/pair-explorer/0x74fb648e0a76831d5a18bf08333603450da7c221"
+                        "https://www.dextools.io/app/en/ether/token/0x74fb648e0a76831d5a18bf08333603450da7c221"
                       );
                     }}
                     className="flex w-[70px] max-w-[70px] bg-white transition-all duration-[100ms] px-3.5 items-center justify-center hover:bg-opacity-[17.5%] cursor-pointer select-none flex bg-opacity-10 rounded-r-lg select-none border-y-2 border-r-2 border-opacity-10 border-white"
                   >
                     <img src="/dextools.svg" className="h-[20px]" />
-                  </div>
+                  </div> */}
                 </div>
               </>
             );
